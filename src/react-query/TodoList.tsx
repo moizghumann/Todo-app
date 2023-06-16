7ae3b60189ee4265ddd259b955c1c07a6826fd9c
moizghumann <moizghuman@gmail.com>
2023-06-17 01:59:23 +0500

    bleh

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -10,20 +10,8 @@ interface Todo {
 
 const endpoint = 'https://jsonplaceholder.typicode.com/';
 
-const TodoList = (endpoint: string) => {
+const TodoList = () => {
 
-  const fetchToDos = () =>
-    axios
-      .get<Todo[]>(endpoint + 'todos')
-      .then(res => res.data)
-
-  // useQuery is used to ask the magical source to find something specific. 
-  const { data: todos } = useQuery({
-    // queryKey is used to define what specific thing we need from magical source
-    queryKey: ['todos'],
-    // queryFn is like a special instruction that tells the magical source how to find the information you want. It's like a map that guides the magical source to the right place.
-    queryFn: fetchToDos
-  })
 
   // if (error) return <p>{error}</p>;
 
