3ccb5e35b0c39ce38c8560445b7cc81addf48026
moizghumann <moizghuman@gmail.com>
2023-06-17 04:02:14 +0500

    add react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 
@@ -12,6 +13,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient} >
       <App />
+      {/* only included in development build */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
