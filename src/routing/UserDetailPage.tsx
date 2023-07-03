357b2654d8d0566c87b188c2db4cd1a54c25c59e
moizghumann <moizghuman@gmail.com>
2023-07-04 01:31:33 +0500

    getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,19 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  // extract parameter values from the URL
+  const params = useParams();
+  console.log(params);
+
+  // setSearchParams should only be called in event handlers and effects since it has a side effect and makes the component impure if used otherwise
+  // Get and update search parameters in the URL
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get('age'));
+
+  // Get the current location object
+  const location = useLocation();
+  console.log(location);  // Prints the current location object representing the URL
+
   return <p>User</p>;
 };
 
